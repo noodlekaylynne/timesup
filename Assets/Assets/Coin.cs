8abe53d26e2c1ac6045a2ebdf7db322199b7f0e3
noodlekaylynne <94488381+noodlekaylynne@users.noreply.github.com>
2023-02-09 10:56:06 -0600

    Gold Coin Sound
    Added a noise upon pick up of a gold coin. Also some small platform changes included.

--- Assets/Assets/Coin.cs
+++ Assets/Assets/Coin.cs
@@ -2,14 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class Coin : MonoBehaviour
 {
+    public AudioSource coinSound;
+
+    void Start()
+    {
+        coinSound = gameObject.GetComponent<AudioSource>();
+    }
+
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
+            coinSound.Play();
+            transform.position = Vector3.one * 1000;
             CoinCounterScript.coinAmount += 1;
-            Destroy(gameObject);
+            Destroy(gameObject, coinSound.clip.length);
+
+            
         }
     }
 }
